4d59ee8baed33e0f7c9ee967e7fbdca88b925b0c
차하연 <chaddol31000@naver.com>
2025-03-14 14:19:03 +0900

    supply 완성

--- supply_view.py
+++ supply_view.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, redirect, request
+from flask import Blueprint, render_template, redirect, request
 
-app = Flask(__name__)
+supp_app = Blueprint('supp_app', __name__)
 
 supplies=[]
 sno=1
 
 
-@app.route("/supply/list")
+@supp_app.route("/supply/list")
 def index_sup():
   return render_template("supply/list.html", supplies=supplies)
 
-@app.route("/supply/add", methods=['GET','POST'])      # 추가 (get: 작성화면보기 / post: 작성후 비품 목록으로 이동)
+@supp_app.route("/supply/add", methods=['GET','POST'])      # 추가 (get: 작성화면보기 / post: 작성후 비품 목록으로 이동)
 def add():
   if request.method=='GET':
     return render_template("supply/add.html")
@@ -24,7 +24,7 @@ def add():
     sno+=1
     return redirect("/supply/list")
   
-@app.route("/supply/read")           # 비품번호를 이용해 항목 출력
+@supp_app.route("/supply/read")           # 비품번호를 이용해 항목 출력
 def read():
   sno = int(request.args.get('sno'))
   print(sno)
@@ -34,7 +34,7 @@ def read():
       return render_template("supply/read.html", supply=s)
   return redirect("/supply/list")
 
-@app.route("/supply/up", methods=['POST'])       # 갯수 증가
+@supp_app.route("/supply/up", methods=['POST'])       # 갯수 증가
 def up():
   sno = int(request.form.get('sno'))
   for s in supplies:
@@ -42,7 +42,7 @@ def up():
       s['count']=s['count']+1
   return redirect("/supply/list")
 
-@app.route("/supply/down", methods=['POST'])     # 갯수 감소
+@supp_app.route("/supply/down", methods=['POST'])     # 갯수 감소
 def down():
   sno = int(request.form.get('sno'))
   for s in supplies:
@@ -51,7 +51,7 @@ def down():
   return redirect("/supply/list")
 
 
-@app.route("/supply/delete", methods=['POST'])   # 삭제
+@supp_app.route("/supply/delete", methods=['POST'])   # 삭제
 def delete():
   sno = int(request.form.get('sno'))
   for s in supplies:
